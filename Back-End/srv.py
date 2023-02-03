6d27c16a7c7f383849ad6fbf48f49e821d3d5ee5
Logi1knobe <68345602+logi1knobe@users.noreply.github.com>
2023-02-03 09:48:40 -0800

    Create srv.py

---
+++ Back-End/srv.py
@@ -0,0 +1,17 @@
+def start():
+  import pygame
+  pygame.init()
+  print("loading...")
+  pygame.time.wait(3000)
+  import NumPy
+  import time
+  import math
+  import hashlib
+  import os
+  import random
+  import sys
+  print("Done")
+
+def pm():
+  x = random.randint(1000000000,99999999999)
+  pass = hashlib.sha3_512(x).hexdigest()

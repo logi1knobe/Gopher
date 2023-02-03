0d37e5136fd77f3217e883fb139e5393fb3efad8
Logi1knobe <68345602+logi1knobe@users.noreply.github.com>
2023-02-03 09:48:38 -0800

    Create pswrdmanger.py

---
+++ Back-End/pswrdmanger.py
@@ -0,0 +1,9 @@
+"""
+If anyone but logan reads this, even if they are a dev, DO NOT TOUCH ANYTHING HERE
+"""
+import srv.py
+
+srv.start()#imports all libraries
+srv.pm()#creates password
+
+print(str(pass)) #temp solution

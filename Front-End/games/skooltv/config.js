6fca684c8459e81bc2d2e3b0891fad4725605542
Logi1knobe <68345602+logi1knobe@users.noreply.github.com>
2023-03-28 09:30:33 -0700

    Add files via upload

---
+++ Front-End/games/skooltv/config.js
@@ -0,0 +1,7 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://tvserver2.wcpss.workers.dev",
+
+  VITE_TMDB_API_KEY: "b030404650f279792a8d3287232358e3",
+  VITE_OMDB_API_KEY: "aa0937c0",
+};

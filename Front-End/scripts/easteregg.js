1603883230d11e294edab7ce21ed63ff6217c32f
Igbalabanov <101612094+Igbalabanov@users.noreply.github.com>
2023-04-18 15:50:39 -0700

    Update easteregg.js

--- Front-End/scripts/easteregg.js
+++ Front-End/scripts/easteregg.js
@@ -115,9 +115,9 @@ function konamiIsDetected() {
 	$(document).ready(function() {
 		
 		$('body').jGravity({ // jGravity works best when targeting the body
-			target: 'everything', // Enter your target critera e.g. 'div, p, span', 'h2' or 'div#specificDiv', or even 'everything' to target everything in the body
+			target: 'span, p', // Enter your target critera e.g. 'div, p, span', 'h2' or 'div#specificDiv', or even 'everything' to target everything in the body
 			ignoreClass: 'noFall', // Specify if you would like to use an ignore class, and then specify the class
-			weight: 25, // Enter any number 1-100 ideally (25 is default), you can also use 'heavy' or 'light'
+			weight: 20, // Enter any number 1-100 ideally (25 is default), you can also use 'heavy' or 'light'
 			depth: 1, // Enter a value between 1-10 ideally (1 is default), this is used to prevent targeting structural divs or other items which may break layout in jGravity
 			drag: true // Decide if users can drag elements which have been effected by jGravity
 		});

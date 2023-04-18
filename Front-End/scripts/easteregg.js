ce5146be09dea8e145ce4ac584e3400d5c533e39
Igbalabanov <101612094+Igbalabanov@users.noreply.github.com>
2023-04-18 15:25:16 -0700

    Update easteregg.js

--- Front-End/scripts/easteregg.js
+++ Front-End/scripts/easteregg.js
@@ -112,8 +112,8 @@
 
 // Function that is invoked after the konami code has been entered
 function konamiIsDetected() {
-	var script = document.createElement("script");
-	script.src = "/scripts/gravity.js";
-	document.body.appendChild(script);
-	void(0);
+	$(document).ready(function() {
+	
+	     $('everything').jGravity();
+	});
 }

dcb693be0061a2578213135ad092cb0f425e1a56
caffeinated squirrel <101604651+HECKER235345345@users.noreply.github.com>
2023-04-27 09:18:18 -0700

    Update main.js

--- Front-End/scripts/main.js
+++ Front-End/scripts/main.js
@@ -5,9 +5,13 @@ if  (CurrentColor == "light") {
     document.getElementById('Cssheet').href='/Themes/Light.css';
     localStorage.setItem("CurrentColor", "light");
 } else {
-    document.getElementById('Cssheet').href='/Themes/Dark.css';
-    localStorage.setItem("CurrentColor", "classic");       
+    document.getElementById('Cssheet').href='/Themes/UltraLight.css';
+    localStorage.setItem("CurrentColor", "UltraLight");       
+} else if {
+    document.getElementById('Cssheet).href='/Themes/Dark.css';
+    localStorage.setItem("CurrentColor", "Dark");
 }
+
 
 
 

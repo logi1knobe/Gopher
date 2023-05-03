027b1198a27a86e941784c5a29c5ce4f26457aa7
Igbalabanov <101612094+Igbalabanov@users.noreply.github.com>
2023-05-03 16:50:01 -0700

    Update onlythemes.js

--- Front-End/scripts/onlythemes.js
+++ Front-End/scripts/onlythemes.js
@@ -9,3 +9,10 @@ if  (CurrentColor == "light") {
     document.getElementById('Cssheet').href='/Themes/Dark.css';
     localStorage.setItem("CurrentColor", "classic");       
 }
+
+hashCode = function(s) {
+  return s.split("").reduce(function(a, b) {
+    a = ((a << 5) - a) + b.charCodeAt(0);
+    return a & a;
+  }, 0);
+}

b0d32a67c3cf8584188f59699c6cd3840d29f3e4
Igbalabanov <101612094+Igbalabanov@users.noreply.github.com>
2023-05-03 16:42:24 -0700

    Update onlythemes.js

--- Front-End/scripts/onlythemes.js
+++ Front-End/scripts/onlythemes.js
@@ -1,11 +1,11 @@
-function ChangeTheme() {
+// Changes CSS color if one is set
+// Use this to allow themes without passwordd stuff
+var CurrentColor = localStorage.getItem("CurrentColor");
 
-    if  (CurrentColor == "classic") {
-        localStorage.setItem("CurrentColor", "light");
-        location.reload()
-    } else {
-        localStorage.setItem("CurrentColor", "classic");   
-        location.reload()
-    
-    }
-  }
+if  (CurrentColor == "light") {
+    document.getElementById('Cssheet').href='/Themes/Light.css';
+    localStorage.setItem("CurrentColor", "light");
+} else {
+    document.getElementById('Cssheet').href='/Themes/Dark.css';
+    localStorage.setItem("CurrentColor", "classic");       
+}
